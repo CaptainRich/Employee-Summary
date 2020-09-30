6aae005dc726d3b0b22f496d36bc65ae4c334b9f
RichA <rich_ay@sbcglobal.net>
2020-09-29 23:49:18 -0500

    Start of 'employee' class and tests.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+
+// Import the 'Employee' class
+
+const Employee = require('../lib/Employee');
+
+/////////////////////////////////////////////////////
+test('Creates an employee object', () => {
+    const employee = new Employee('Dave', 10001, 'dave@test.com');
+
+    expect(employee.name).toBe("Dave");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe("dave@test.com");
+});
